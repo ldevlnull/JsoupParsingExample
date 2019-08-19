dd6a6e48c5836eef87019cc5596247557673a6e4
= <=>
2019-08-19 19:36:59 +0300

    Program now parses all the data about matches we need

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -7,6 +7,7 @@ import java.io.IOException;
 import java.sql.Time;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.stream.Collectors;
 
 public class Main {
 
@@ -19,28 +20,46 @@ public class Main {
                     .referrer("http://google.com").get();
 
             Elements elements = doc.getElementsByClass("upcoming-match");
-            List<MatchModel> matchModelList = new ArrayList<MatchModel>();
+            List<MatchModel> matchModelList = new ArrayList<>();
 
             for (Element element: elements) {
-                MatchModel model = new MatchModel();
-                String t = element
-                        .getElementsByAttribute("data-time-format")
-                        .get(0).text();
+                if(!element.getElementsByClass("map-text").text().equalsIgnoreCase("")) {
+                    String time = element
+                            .getElementsByAttribute("data-time-format")
+                            .get(0).text();
+                    String teams[] = element.getElementsByClass("team")
+                            .stream().map(Element::text)
+                            .collect(Collectors.toList()).toArray(new String[]{});
 
-                int hours = Integer.parseInt(t.substring(0, 2));
-                int minutes = Integer.parseInt(t.substring(3, 5));
-                int millieSec = hours*3600*1000 + minutes*60*1000;
+                    MatchModel model = new MatchModel();
+                    model.setTime(formatTime(time));
+                    model.setType(element.getElementsByClass("map-text").text());
+                    model.setTournament(element.getElementsByClass("event-name").text());
 
-                model.setTime(new Time(millieSec));
+                    try {
+                        model.setTeam1(teams[0]);
+                        model.setTeam2(teams[1]);
+                    } catch (IndexOutOfBoundsException e) {
+                        model.setTeam1("No data");
+                        model.setTeam2("No data");
+                    }
 
-                matchModelList.add(model);
+                    matchModelList.add(model);
+                }
             }
 
-            matchModelList.forEach(e ->
-                    System.out.println(e.getTime().toLocalTime()));
+            matchModelList.forEach(System.out::println);
 
         } catch (IOException e){
             System.out.println("Document not found");
         }
+    }
+
+    private static Time formatTime(String time){
+        int hours = Integer.parseInt(time.substring(0, 2));
+        int minutes = Integer.parseInt(time.substring(3, 5));
+        int millieSec = hours*3600*1000 + minutes*60*1000;
+
+        return new Time(millieSec);
     }
 }
